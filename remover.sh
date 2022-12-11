856417adc7607251016c0499bdcbbb9d912bcc68
forveined <120341546+forveined@users.noreply.github.com>
2022-12-11 18:03:01 -0500

    Update remover.sh

--- remover.sh
+++ remover.sh
@@ -14,8 +14,8 @@ if [ "$input" != "y" ]; then
   exit 1
 fi
 
-sudo /usr/local/bin/jamf/removeMdmProfile; then
+sudo /usr/local/bin/jamf/removeMdmProfile
 echo "MDM Profile Removed"
-sudo /usr/local/bin/jamfremoveFramework; then
+sudo /usr/local/bin/jamfremoveFramework
 echo "Framework Removed"
 echo"To use blocked software you need to move it into the applications folder"
