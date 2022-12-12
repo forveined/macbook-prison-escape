28755f346a6ec2a315d17f75f784780f0a0daaa8
forveined <120341546+forveined@users.noreply.github.com>
2022-12-11 21:56:05 -0500

    Update keysystem.sh

--- keysystem.sh
+++ keysystem.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-
 echo "Enter the key to access the script:"
 read key
 
 
-if [ "$key" == "234567, 123456" ]; then
+if [ -n "$key" ] && echo "234567, 123456" | grep -q "$key"; then
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/forveined/macbook-prison-escape/main/remover.sh)"
-
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/forveined/macbook-prison-escape/main/remover.sh)"
 else
 
   echo "Incorrect key. Access denied."
-  fi
+fi
+
 exit 1
