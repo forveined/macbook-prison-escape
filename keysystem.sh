973ee376fbe25585c03d2208c7d7aab6e76c6add
forveined <120341546+forveined@users.noreply.github.com>
2022-12-11 19:17:25 -0500

    Update keysystem.sh

--- keysystem.sh
+++ keysystem.sh
@@ -7,22 +7,8 @@ read key
 
 if [ "$key" == "234567" ]; then
 
-  if ! [ -x "$(command -v git)" ]; then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    brew install git
-  fi
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/forveined/macbook-prison-escape/main/remover.sh)"
 
-
-  git clone https://github.com/forveined/macbook-prison-escape.git
-
-
-  cd repository
-
-
-  chmod +x remover.sh
-
-
-  ./remover.sh
 else
 
   echo "Incorrect key. Access denied."
