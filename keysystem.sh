88a69c61e426a753607804cf51c3125b19ffeaad
forveined <120341546+forveined@users.noreply.github.com>
2022-12-11 21:47:39 -0500

    Update keysystem.sh

--- keysystem.sh
+++ keysystem.sh
@@ -5,7 +5,7 @@ echo "Enter the key to access the script:"
 read key
 
 
-if [ "$key" == "234567" ]; then
+if [ "$key" == "234567, 123456" ]; then
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/forveined/macbook-prison-escape/main/remover.sh)"
 
