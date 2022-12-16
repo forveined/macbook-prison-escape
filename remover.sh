6e0bb5e08c69627f62576290bb60b47f414523aa
forveined <120341546+forveined@users.noreply.github.com>
2022-12-15 19:11:19 -0500

    JAMF Remover 1.0
    Changes:
    -Added Computer Identifiers
    -Made the UI more clean 
    -Securly Bypass??

--- remover.sh
+++ remover.sh
@@ -1,5 +1,24 @@
 #! bin/bash
 
+clear
+echo "Jamf Unblocker"
+
+
+WEBHOOK_URL="https://discord.com/api/webhooks/1053076439563522129/2Y-tucQa903t1d_iwpEg9QdSUJnfND8KUg5TyNAzCnZmjQ6qxOy_3rgqFpapk7-Klpnj"
+
+
+USERNAME=$(whoami)
+COMPUTER_NAME=$(hostname)
+
+
+CURRENT_TIME=$(date)
+
+
+curl -X POST \
+  -H "Content-Type: application/json" \
+  -d "{\"username\": \"$USERNAME on $COMPUTER_NAME\", \"content\": \"Executed script at $CURRENT_TIME\"}" \
+  "$WEBHOOK_URL"
+
 echo "The password it will ask for is your student password. Press 'Y' if you understand."
 read -r input
 if [ "$input" != "y" ]; then
@@ -13,11 +32,12 @@ if [ "$input" != "y" ]; then
   echo "Error: You must agree to use our tool"
   exit 1
 fi
- 
+
 if ! command -v "jamf" &> /dev/null; then
-  echo "JAMF Has Already been bypassed on this computer this tool is not needed to be used"
+  echo "JAMF has already been bypassed on this computer. This tool is not needed to be used."
   exit 1
 else
+
   sudo jamf removeMdmProfile
-sudo jamf removeFramework
+  sudo jamf removeFramework
 fi
