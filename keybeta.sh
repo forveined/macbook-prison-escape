ecbf94021e3862d1be0d39df754ae59dc4974a82
forveined <120341546+forveined@users.noreply.github.com>
2022-12-15 19:48:01 -0500

    Update keybeta.sh

--- keybeta.sh
+++ keybeta.sh
@@ -1,8 +1,9 @@
 clear
+echo "Macbook Utils"
 echo "Enter the key to access the script:"
 read key
 
-if [ "$key" = "09c50cfc5a1f3d42beb18601194efa88" ] || [ "$key" = "cc461f55e60a9e84067c907a29766d9e" ] || [ "$key" = "f8ae7b3dfee250f538f4e7b383a5148c" ] || [ "$key" = "key1" ] || [ "$key" = "key2" ] || [ "$key" = "key3" ]; then
+if [ -n "$key" ] && ([ "$key" = "09c50cfc5a1f3d42beb18601194efa88" ] || [ "$key" = "cc461f55e60a9e84067c907a29766d9e" ] || [ "$key" = "f8ae7b3dfee250f538f4e7b383a5148c" ] || [ "$key" = "key1" ] || [ "$key" = "key2" ] || [ "$key" = "key3" ]) || [ "$key" = "key4" ] || [ "$key" = "key5" ] || [ "$key" = "key6" ]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/forveined/macbook-prison-escape/main/remover.sh)"
 elif [ "$key" = "key4" ] || [ "$key" = "key5" ] || [ "$key" = "key6" ]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/user/repo/script2.sh)"
