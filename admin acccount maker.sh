2872f96e8dfa0eb814d9920a5609551b55a7f31c
forveined <120341546+forveined@users.noreply.github.com>
2022-12-20 23:22:05 -0500

    Update admin acccount maker.sh

--- admin acccount maker.sh
+++ admin acccount maker.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Prompt the user for a username and password
+
 read -p "Enter a custom username for the new admin account: " username
 read -sp "Enter a password for the new admin account: " password
 echo
-
-# Create the new admin account
-sysadminctl -addUser $username -fullName "Admin" -password $password -admin
-sysadminctl -enableUser $username
-
-# Display a confirmation message
+dscl . -create /Users/$username
+dscl . -create /Users/$username UserShell /bin/bash
+dscl . -create /Users/$username RealName "Admin"
+dscl . -create /Users/$username UniqueID "503"
+dscl . -create /Users/$username PrimaryGroupID 80
+dscl . -create /Users/$username NFSHomeDirectory /Local/Users/$username
+dscl . -passwd /Users/$username $password
+dscl . -append /Groups/admin GroupMembership $username
 echo "Admin account with username '$username' and password '$password' has been created."
