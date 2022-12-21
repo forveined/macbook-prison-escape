aa61fe5820d1336cce3962403ceb180511bab73e
forveined <120341546+forveined@users.noreply.github.com>
2022-12-20 23:18:42 -0500

    Update admin acccount maker.sh

--- admin acccount maker.sh
+++ admin acccount maker.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-clear
-printf "\033]0;Admin Account Maker\007"
-echo "Enter a custom username for the new admin account:"
-read username
-echo "Enter a password for the new admin account:"
-read -s password
+
+# Prompt the user for a username and password
+read -p "Enter a custom username for the new admin account: " username
+read -sp "Enter a password for the new admin account: " password
+echo
+
+# Create the new admin account
 sysadminctl -addUser $username -fullName "Admin" -password $password -admin
 sysadminctl -enableUser $username
+
+# Display a confirmation message
 echo "Admin account with username '$username' and password '$password' has been created."
